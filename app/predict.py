e92c1d3811cf7473761283e21b92fac6cd03c803
Borniaa <alayabornia@gmail.com>
2025-07-20 12:16:25 +0100

    Fix model path for Streamlit deployment

--- app/predict.py
+++ app/predict.py
@@ -1,9 +1,11 @@
+import os
 import joblib
 import numpy as np
 
 # Load model and scaler
 model = joblib.load('app/rf.pkl')
 scaler = joblib.load('app/scaler.pkl')
+model_path= 0s.path.join(os.path.dirname(__file__), 'app/rf.pkl')
 
 def predict_potability(features: list):
     data = np.array(features).reshape(1, -1)
